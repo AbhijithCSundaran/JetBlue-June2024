72569730fb39268f4e76d76a03e34aeb35ca2906
AbhijithCSundaran <abhijith.sundaran@annalect.com>
2024-07-04 18:42:24 +0530

    js update

--- gradient/richLoads/jetblue_gradient_300x600_richload/main.js
+++ gradient/richLoads/jetblue_gradient_300x600_richload/main.js
@@ -30,12 +30,13 @@ function animate() {
   // let video play
   tl.addLabel('start', 0)
     .to('#container', 0.5, { autoAlpha: 1 }, 'start+=0.5')
-    .staggerTo(['#copy1-1', '#copy1-2', '#copy1-3'], 0.8, { autoAlpha: 1, ease: "expo.out" }, 0.1, 'start+=2')
-    .staggerFrom(['#copy1-1', '#copy1-2', '#copy1-3'], 0.8, { y: '+=50', ease: "expo.out" }, 0.1, 'start+=2')
+    .staggerTo(['#copy1-1', '#copy1-2', '#copy1-3'], 0.8, { autoAlpha: 1, ease: Power2.easeOut }, 0.1, 'start+=2')
+    .staggerFrom(['#copy1-1', '#copy1-2', '#copy1-3'], 0.8, { y: '+=50', ease: Power2.easeOut }, 0.1, 'start+=2')
     .to(['#copy1', '#activity'], 0.5, { alpha: 0, delay: 2 })
-    .staggerTo(['#copy2-1', '#copy2-2', '#copy2-3'], 0.8, { autoAlpha: 1, ease: "expo.out" }, 0.1, 'start+=5.5')
-    .staggerFrom(['#copy2-1', '#copy2-2', '#copy2-3'], 0.8, { y: '+=50', ease: "expo.out" }, 0.1, 'start+=5.5')
-    .to('#endframe', 1, { top: 0, ease: "expo.out", delay: 1.5 })
+    .staggerTo(['#copy2-1', '#copy2-2', '#copy2-3'], 0.8, { autoAlpha: 1, ease: Power2.easeOut }, 0.1, 'start+=5.5')
+    .staggerFrom(['#copy2-1', '#copy2-2', '#copy2-3'], 0.8, { y: '+=50', ease: Power2.easeOut }, 0.1, 'start+=5.5')
+    .to('#logo', 0.4, { alpha: 0, delay: 0.0, ease: Power2.easeOut, delay: 2.35 })
+    .to('#endframe', 1, { top: 0, ease: Power2.easeOut, delay: 0.0 })
     .to('#shine', 0.5, { backgroundPosition: '180px -35px' });
 }
 
