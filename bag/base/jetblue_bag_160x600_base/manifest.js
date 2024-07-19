a8b17d9f6e137f4ce5a2f98af99f08cb45e650db
AbhijithCSundaran <161324727+AbhijithCSundaran@users.noreply.github.com>
2024-07-19 23:27:11 +0530

    banners added with meta

---
+++ bag/base/jetblue_bag_160x600_base/manifest.js
@@ -0,0 +1,14 @@
+FT.manifest({
+    "filename": "index.html",
+    "width": 160,
+    "height": 600,
+    "clickTagCount": 1,
+    "hideBrowsers": ["ie8"],
+    "richloads": [
+ 		{"name":"main_rl", "src":"JetBlue_Display Banners_FCB_V1_Branded_HTML5_160x600"}
+ 	],
+    "instantAds": [
+        {"name":"main_rl",          "type":"richload"}, 
+        {"name":"clickTag1_url",    "type":"text", "default":"http://www.landingpageurl.com/?blahblah"}
+    ]
+});
